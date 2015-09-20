69e100f1c204f1f594205ce30fc5498ad7c87696
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-09-20 14:54:53 +0900

    Switch state to a list of possible choices

---
+++ position/migrations/0002_auto_20150920_0548.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('position', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='location',
+            name='state',
+            field=models.CharField(choices=[('', 'Unselected'), ('entered', 'Entered an Area'), ('exited', 'Exited an Area'), ('Do Button', 'Test Entry')], max_length=2),
+        ),
+    ]
--- position/models.py
+++ position/models.py
@@ -6,4 +6,12 @@ class Location(models.Model):
     created = models.DateTimeField(default=now)
     label = models.TextField()
     location = models.TextField()
-    state = models.TextField()
+    state = models.CharField(
+        max_length=16,
+        choices=(
+            ('', 'Unselected'),
+            ('entered', 'Entered an Area'),
+            ('exited', 'Exited an Area'),
+            ('Do Button', 'Test Entry'),
+        )
+    )
