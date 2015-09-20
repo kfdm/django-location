6b5b6ff22482bd539def458e41a0e2ae78087eb2
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-09-20 14:45:14 +0900

    Add initial migration

---
+++ position/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Location',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', models.DateTimeField(default=django.utils.timezone.now)),
+                ('label', models.TextField()),
+                ('location', models.TextField()),
+                ('state', models.TextField()),
+            ],
+        ),
+    ]
