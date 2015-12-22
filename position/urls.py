c596dd7917aa83d94a2cf8e48ca19cfa22aedc89
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-12-22 11:13:33 +0900

    Update urls for Django 1.9

--- position/urls.py
+++ position/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import TemplateView
 
 import position.views
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^location$', position.views.LocationPattern.as_view(), name='location'),
     url(r'^timeline', TemplateView.as_view(template_name="location_timeline.html")),
     url(r'^calendar$', position.views.LocationCalendarView.as_view(), name='calendar'),
-)
+]
